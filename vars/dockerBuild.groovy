0c49bb1e8ec63af65c88cc86ac7915a9caf8e064
isekai-003 <128886034+isekai-003@users.noreply.github.com>
2023-09-22 20:16:22 +0000

    doc

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,8 +2,8 @@ def call(String project, String ImageTag,String user) {
    
        sh """
               docker image build -t ${user}/${project} . 
-              docker image tag ${user}/${project} ${hubUser}/${project}:${ImageTag}
-              docker image tag ${user}/${project} ${hubUser}/${project}:latest
+              docker image tag ${user}/${project} ${user}/${project}:${ImageTag}
+              docker image tag ${user}/${project} ${user}/${project}:latest
           """
 
        
